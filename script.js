17e55b8dbbc6d476f718dd7b5de39e5a676b2d93
NupuK13 <nandanikumar72111@gmail.com>
2025-06-01 22:50:53 +0530

    change navbar

--- script.js
+++ script.js
@@ -17,18 +17,10 @@ products.forEach(product => {
     <img src="${product.image}" alt="${product.name}" class="w-full h-48 object-cover rounded mb-2">
     <h3 class="text-lg font-semibold">${product.name}</h3>
     <p class="text-gray-700">₹${product.price}</p>
-    <button onclick="addToCart(${product.id})" class="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-      Add to Cart
+    <button onclick="addToCart(${product.id})" class="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"><add to cart>
     </button>
-  `;
+    `;
 
-  productGrid.appendChild(card);
+   productGrid.appendChild(card);
+
 });
-
-function addToCart(productId) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const product = products.find(p => p.id === productId);
-  cart.push(product);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(`${product.name} added to cart!`);
-}
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+import tailwindcssVite from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [tailwindcssVite()],
+});
